dcdcbe0c2fd25e359e64860c647eeed5e43acc98
Júlio De Bastiani <julioc.debastiani@gmail.com>
2022-02-28 09:08:14 -0300

    add third party libs

--- src/ucb/frontend/lexer.hpp
+++ src/ucb/frontend/lexer.hpp
@@ -2,9 +2,25 @@
 
 #include <ucb/core/config.hpp>
 
+#include <string>
+#include <iostream>
+
 #include <ucb/frontend/span-reader.hpp>
 
 namespace ucb::frontend
 {
-    
+    class Lexer
+    {
+    public:
+        Lexer(std::string filename, bool print_debug):
+            _filename(std::move(filename)),
+            _print_debug(print_debug)
+        {
+            std::cout << "batatas! " << _filename << std::endl;
+        }
+
+    private:
+        std::string _filename;
+        bool _print_debug;
+    };
 }
--- src/ucb/uic/main.cpp
+++ src/ucb/uic/main.cpp
@@ -4,8 +4,43 @@
 
 #include <ucb/frontend/parser.hpp>
 
+#include <boost/program_options.hpp>
+
+using namespace ucb;
+
+namespace po = boost::program_options;
+
 int main(int argc, char **argv)
 {
-    std::cout << "aaaaaaa" << std::endl;
-    return 0;
+    std::string input_file;
+
+    po::options_description desc("UCB Intermediate Representaiton Compiler");
+    desc.add_options()
+        ("help,h", "print this message")
+        ("input-file", po::value<std::string>(&input_file)->required(), "file to be compiled")
+    ;
+
+    po::positional_options_description p;
+    p.add("input-file", -1);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+
+    if (vm.count("help"))
+    {
+        std::cout << desc << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    if (!vm.count("input-file"))
+    {
+        std::cout << "an input file must be provided" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    po::notify(vm);
+
+    std::cout << vm["input-file"].as<std::string>() << std::endl;
+
+    return EXIT_SUCCESS;
 }
